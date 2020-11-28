663caead0de64175505c73b9e5a50d3ce38fbd8c
tdxa <anndoman3@gmail.com>
2020-11-28 20:24:44 +0100

    Connect a database

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+
+//database config
+const db = require('./config/keys').MongooseURI;
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("MonoDB connected"))
+    .catch(err => console.log(err));
+
 
 //routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
-const expressLayout = require('express-ejs-layouts')
+const expressLayout = require('express-ejs-layouts');
+const { Mongoose } = require('mongoose');
 
 //ejs
 app.use(expressLayout);
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on ${port} port!`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongooseURI: 'mongodb+srv://tdxa:tdxa123@my-cluster.oc4rz.mongodb.net/login_app?retryWrites=true&w=majority'
+}
