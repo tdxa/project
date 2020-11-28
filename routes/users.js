e50fb3427666813feb86633e108ec999ef3c2e4c
tdxa <anndoman3@gmail.com>
2020-11-28 19:41:19 +0100

    Create a register view

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router();
 
 //Login Page
-router.get('/login',(req,res) => res.send('Login'))
+router.get('/login',(req,res) => res.render('login'))
 
 //Register page
-router.get('/register',(req,res) => res.send('Register'))
+router.get('/register',(req,res) => res.render('register'))
 module.exports = router;
