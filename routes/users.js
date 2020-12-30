6a9bd26190f9c3e1a8a34a7c196e6332edc61211
tdxa <anndoman3@gmail.com>
2020-12-30 19:27:07 +0100

    Add logout

--- routes/users.js
+++ routes/users.js
@@ -81,12 +81,21 @@ router.post('/register', (req, res) => {
 });
 
 //Login handle
-router.post('/login', (req, res,next) => {
+router.post('/login', (req, res, next) => {
     passport.authenticate('local', {
         successRedirect: '/dashboard',
         failureRedirect: '/users/login',
         failureFlash: true
     })(req, res, next);
 });
+
+
+// Logout handle
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'You have been successfully logged out');
+
+    res.redirect('/users/login');
+})
 
 module.exports = router;
