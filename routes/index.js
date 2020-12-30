a052caab154056ea50e672d10b99dbd646ead971
tdxa <anndoman3@gmail.com>
2020-12-30 19:41:26 +0100

    Protect dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth')
 
 // main page
-router.get('/',(req,res) => res.render('welcome'));
+router.get('/', (req, res) => res.render('welcome'));
 
 // dashboard
-router.get('/dashboard',(req,res) => res.render('dashboard'));
-
-
+router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard'));
 
 module.exports = router;
