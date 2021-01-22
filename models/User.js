384f96a9011305c4587ebc5eb7a253e080a5830e
IamCostello <mynameiscostello@gmail.com>
2021-01-23 00:39:47 +0100

    update models

--- models/User.js
+++ models/User.js
@@ -1,31 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
-    name:{
-
-        type: String,
-        required: true
-        
-    },
-    email:{
-
-        type: String,
-        required: true
-
-    },
-    password:{
-
-        type: String,
-        required: true
-
-    },
-    date:{
-
-        type: Date,
-        default: Date.now
-
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  userPosts: {
+    posts: [
+      {
+        postId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Post",
+        },
+      },
+    ],
+  },
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
