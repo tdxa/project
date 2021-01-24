6776b1ba3272c2f33342d37736a5716dce043940
IamCostello <mynameiscostello@gmail.com>
2021-01-24 03:31:06 +0100

    update user model

--- models/User.js
+++ models/User.js
@@ -16,17 +16,6 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
-  userPosts: {
-    posts: [
-      {
-        postId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Post",
-        },
-        qnty: Number,
-      },
-    ],
-  },
 });
 
 const User = mongoose.model("User", UserSchema);
