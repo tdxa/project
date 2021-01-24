50dbe4daaf8ca26a443d2722f980293fdee8b5fb
tdxa <anndoman3@gmail.com>
2021-01-24 03:49:51 +0100

    edit multer

--- app.js
+++ app.js
@@ -55,13 +55,18 @@ const fileStorage = multer.diskStorage({
   },
 });
 
-// const allowedMimeTypes = ["image/png", "image/jpg"];
-// const fileFilter = (req, file, cb) => {
-//   const allowedFile = allowedMimeTypes.includes(file.mimetype);
-//   cb(null, allowedFile);
-// };
+const allowedMimeTypes = ["image/png", "image/jpg","image/jpeg"];
+const fileFilter = (req, file, cb) => {
+  const allowedFile = allowedMimeTypes.includes(file.mimetype);
+  cb(null, allowedFile);
+};
 
-app.use(multer({ storage: fileStorage }).single("postImage"));
+
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("postImage")
+);
+
+// app.use(multer({ storage: fileStorage }).single("postImage"));
 
 //Global variables
 app.use((req, res, next) => {
